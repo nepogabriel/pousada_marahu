d41bb717689df00765322ac19b397c102d1dbe2a
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2022-03-19 01:13:41 -0300

    DB- Tabela usuario criada pelas migrations

---
+++ database/migrations/2022_03_19_031500_create_usuario_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 60);
+            $table->string('email', 60);
+            $table->string('senha', 255);
+            $table->string('cpf', 11);
+            $table->string('telefone', 12);
+            $table->boolean('nivel')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
