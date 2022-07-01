ca2493c3fcbd4add608986186ebfad4265a1500e
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2022-06-30 21:37:29 -0300

    PTM-12 - Calculo de 2 adultos está pronto

--- app/Http/Controllers/api/ReservationController.php
+++ app/Http/Controllers/api/ReservationController.php
@@ -13,35 +13,29 @@ class ReservationController extends Controller
     {
         // Data atual
         $currentDate = date('Y-m-d');
-        //$currentDate = Carbon::now();
-
-        // Data de entrada
-//        $entry_date_format = Carbon::createFromFormat('Y-m-d', $request->entry_date);
-//        $entry_date = $entry_date_format->format('Y-m-d');
-
         $entry_date = $request->start_date;
 
-
         // Data de saída
-//        $exit_date_format = Carbon::createFromFormat('Y-m-d', $request->exit_date);
-//        $exit_date = $exit_date_format->format('Y-m-d');
-
         $exit_date = $request->end_date;
 
-        if($entry_date >= $currentDate && $exit_date > $currentDate && $entry_date < $exit_date) {
+        if(($entry_date >= $currentDate) && ($exit_date > $currentDate) && ($entry_date < $exit_date)) {
 
+            // Calculo para 2 diárias - Adulto
             if ($request->adults == 2) {
-                $diaria = 5;
-                $value = 300;
-
-                $valueTotal = $diaria * $value;
-
-                return response()->json(['Valor total' => $valueTotal], 200);
+                $result = $this->calcuteTwoPeopleMarahu($request->hotelRate, $request->value);
             }
 
-            // Lógica do restante das diárias
+            // todo Lógica do restante das diárias
+
+            return $result;
         }
 
         return response()->json(['message' => 'Desculpe! Escolha uma data válida!'], 500);
+    }
+
+    private function calcuteTwoPeopleMarahu($hotelRate, $value) {
+        $valueTotal = $hotelRate * $value;
+
+        return response()->json(['Valor total' => $valueTotal], 200);
     }
 }
